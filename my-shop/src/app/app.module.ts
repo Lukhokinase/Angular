1c3abb55d2d402427f355e41d7c2fb056146337c
Aphelele Ndlela <aphelelendlela95@gmail.com>
2023-06-21 15:35:56 +0200

    Your-bag

--- my-shop/src/app/app.module.ts
+++ my-shop/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { YourBagComponent } from './your-bag/your-bag.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    YourBagComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
