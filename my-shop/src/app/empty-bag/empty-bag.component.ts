42e79bc86e9fdf9a93373f6dc1c6e146b8de145c
LNdindwa <lndindwa@gmail.com>
2023-06-21 15:36:08 +0200

    empty-bag

---
+++ my-shop/src/app/empty-bag/empty-bag.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-bag',
+  templateUrl: './empty-bag.component.html',
+  styleUrls: ['./empty-bag.component.css']
+})
+export class EmptyBagComponent {
+
+}
