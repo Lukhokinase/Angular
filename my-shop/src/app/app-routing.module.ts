56e75fcf30d4b6ccccfdf018fc6effd2ec21cb61
Ncediswa Tabule <ncediswatabule.tabule399@gmail.com>
2023-06-22 14:38:02 +0200

    updating components

--- my-shop/src/app/app-routing.module.ts
+++ my-shop/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 // import { MenuComponent } from './menu/menu.component';
 import { MenuofGodComponent } from './menuof-god/menuof-god.component'
 import { ViewoneComponent } from './viewone/viewone.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 const routes: Routes = [
   // { path: 'menu', component: MenuComponent },
   { path: 'godmenu', component: MenuofGodComponent},
   { path: 'view', component: ViewoneComponent},
+  { path: "checkout", component: CheckoutComponent }
 
 
 ];
--- my-shop/src/app/checkout/checkout.component.ts
+++ my-shop/src/app/checkout/checkout.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-checkout',
+  selector: 'app-root',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent {
+  stripe: any | null = null;
 
+  PRODUCT_SKU: string = "sku_H5hfH3emeikvXY";
+
+  PUBLISHABLE_KEY: string = "pk_test_DMcLz3qQcHzlWOhTZbiIZXhY00EHVCWLY7";
+
+  async ngOnInit(): Promise<void> {
+    this.stripe = await loadStripe(this.PUBLISHABLE_KEY);
+  }
+
+  checkout(): void {
+    this.stripe.redirectToCheckout({
+      items: [
+        { sku: this.PRODUCT_SKU, quantity: 1 }
+      ],
+      successUrl: "https://example.com/success",
+      cancelUrl: "https://example.com/cancel"
+    });
+  }
+}
+function loadStripe(PUBLISHABLE_KEY: string): any {
+  throw new Error('Function not implemented.');
 }
