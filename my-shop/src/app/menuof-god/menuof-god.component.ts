eddcb960017a2b734ac0e5bc0dd1a2c43920262e
Aphelele Ndlela <aphelelendlela95@gmail.com>
2023-06-23 15:49:05 +0200

    added product service functions

--- my-shop/src/app/menuof-god/menuof-god.component.ts
+++ my-shop/src/app/menuof-god/menuof-god.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../Item';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-menuof-god',
   templateUrl: './menuof-god.component.html',
   styleUrls: ['./menuof-god.component.css']
 })
-export class MenuofGodComponent {
+export class MenuofGodComponent implements OnInit {
+  products: Item[]= []
+  title=""
+  constructor(private productService: ProductsService){
 
+  }
+  ngOnInit(): void {
+ 
+  }
+
+  getAllItems(){
+    this.productService.getAllItems().subscribe((data)=>{
+      this.products = data
+    })
+  }
 }
--- my-shop/src/app/services/products.service.ts
+++ my-shop/src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Item } from '../Item';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map } from 'rxjs';
 
 
 const apiUrl = 'http://localhost:3200/v1/items'
@@ -11,6 +11,7 @@ const apiUrl = 'http://localhost:3200/v1/items'
 
 
 export class ProductsService {
+  Url: any;
   
 
   constructor(private http: HttpClient) { }
@@ -19,7 +20,15 @@ export class ProductsService {
   getAllItems(): Observable<Item[]> {
     return this.http.get<Item[]>(apiUrl+'/products/getall')
       
-  }
+  };
   
-}
+  getOne(id:any): Observable<Item> {
+    return this.http.get<Item>(apiUrl + '/:id')
+  }
 
+  deleteOne(id: any):Observable<any>{
+    return this.http.get<any>(apiUrl + '/:id')
+  }
+ 
+
+    }
