faadca13422db654f7eeaef358545bbeb0607e52
LNdindwa <lndindwa@gmail.com>
2023-06-23 16:15:26 +0200

    lazy Work

--- my-shop/src/app/empty-bag/empty-bag.component.ts
+++ my-shop/src/app/empty-bag/empty-bag.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
 
 @Component({
-  selector: 'app-empty-bag',
-  templateUrl: './empty-bag.component.html',
-  styleUrls: ['./empty-bag.component.css']
+selector: 'app-root',
+templateUrl: './empty-cart.component.html',
+styleUrls: ['./empty-cart.component.css']
 })
-export class EmptyBagComponent {
+export class EmptyCartComponent implements OnInit {
+
+constructor(private cartService: CartService) {}
+
+ngOnInit() {
+this.cartService.getCart().subscribe(cart => {
+if (cart.items.length === 0) {
+this.isCartEmpty = true;
+} else {
+this.isCartEmpty = false;
+}
+});
+}
+
+amountTab() {
+  return this.cartService.getCart().pipe(
+    // Calculate the total amount of the cart
+    map(cart => cart.totalAmount),
+    // Format the amount as a currency
+    map(amount => `${amount} USD`)
+  );
+}
 
 }
