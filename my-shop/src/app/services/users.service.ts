f7064dde6f84e9a5b5281bda65231d4fed44b35f
Aphelele Ndlela <aphelelendlela95@gmail.com>
2023-07-07 15:48:09 +0200

    reg-done

--- my-shop/src/app/services/users.service.ts
+++ my-shop/src/app/services/users.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 
-const auth_api = 'https://mvc-phunga-git-main-aphelelendlela.vercel.app/pi/auth'
+const auth_api = 'https://mvc-phunga-git-main-aphelelendlela.vercel.app/api/auth'
 const User_api = 'https://mvc-phunga-git-main-aphelelendlela.vercel.app/v2/users/'
 
 
@@ -22,16 +22,17 @@ export class UsersService {
     this.user.push(user);
   }
  
-  // login(id:any): Observable<User> {
-  //   return this.http.get<User>(apiUrl+ '/signin')
-  // }
-  login(user: User) {
-    this.user.push(user)
+  login(id:any): Observable<User> {
     return this.http.get<User>(auth_api+ '/signin')
   }
 
+  // login(user: User) {
+  //   this.user.push(user)
+  //   return this.http.get<User>(auth_api+ '/signin')
+  // }
+
   deleteOne(id: any):Observable<any>{
-    return this.http.get<any>(User_api + '/:id')
+    return this.http.get<any>(auth_api + '/:id')
   }
 
 }
